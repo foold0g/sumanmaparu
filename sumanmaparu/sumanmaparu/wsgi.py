b72597430e525838c8629f119f9366d85d8b1e17
Suman Maparu <maparus@ryanair.com>
2018-09-19 16:15:08 +0100

    changed project structure

--- sumanmaparu/sumanmaparu/wsgi.py
+++ sumanmaparu/sumanmaparu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chaos project.
+WSGI config for sumanmaparu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chaos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sumanmaparu.settings')
 
 application = get_wsgi_application()
