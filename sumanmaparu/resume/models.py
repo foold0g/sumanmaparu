da7f52a9a64d7ee6e5352432e49ff786c32db580
Suman Maparu <maparus@ryanair.com>
2018-09-19 17:32:36 +0100

    Amending DB tables

--- sumanmaparu/resume/models.py
+++ sumanmaparu/resume/models.py
@@ -7,3 +7,6 @@ class Person(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   company = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.first_name +' '+ self.last_name +' '+ self.company
