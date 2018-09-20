40a638ae490366ba5c0e0f8592b766ed4087bc36
Suman Maparu <maparus@ryanair.com>
2018-09-20 17:40:17 +0100

    Adding nw pages

--- sumanmaparu/resume/urls.py
+++ sumanmaparu/resume/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('<str:f_name>/', views.bio, name='bio'),
 ]
--- sumanmaparu/resume/views.py
+++ sumanmaparu/resume/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse("Resume - Suman Maparu")
 
+def bio(request, f_name):
+  return HttpResponse("You're viewing the resume of %s" % f_name)
+
