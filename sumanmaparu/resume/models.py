346a599103ec7d5d000e767ee1ee7f3862f7a195
foold0g <42898776+foold0g@users.noreply.github.com>
2018-09-22 17:49:16 +0100

    Development (#1)
    * Adding profile pic
    
    * Adding git ignore + removing the .idea files from development branch
    
    * Adding Chaos Inc label to gitignore file
    
    * Adding idea directory to git ignore
    
    * Testing gitignore

--- sumanmaparu/resume/models.py
+++ sumanmaparu/resume/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
-
 
 class Person(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   company = models.CharField(max_length=30)
+  profile_image = models.ImageField(upload_to='profile_pic', blank=True)
 
   def __str__(self):
     return self.first_name +' '+ self.last_name +' '+ self.company
